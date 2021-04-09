9741c377a1a0dd5a863133dc37edfe7f970ff5b3
Olga Kulik <kulik_olla@mail.ru>
2021-04-09 14:51:34 +0300

    new structure app

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}

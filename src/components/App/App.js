7f1091708bff5a1b75ea88e452ced6fc1b4be3f8
Olga Kulik <kulik_olla@mail.ru>
2021-04-09 15:17:20 +0300

    added new pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,22 @@
 import styles from './App.module.scss';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { HomePage, FavouritesPage } from '../../pages';
 
 export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <>
+      <Router>
+        <header className="">
+          header
+        </header>
+        <main>
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/favourites" exact component={FavouritesPage} />
+          </Switch>
+        </main>
+      </Router>
+    </>
   );
 }
